b8cc793bf761ce8dd7d3f9c12b30c0e07225d6e9
1 <1@DESKTOP-KA7BF0A>
2022-03-07 13:03:18 +0800

    添加项目文件。

---
+++ 源.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+int main()
+{
+	//char ch;
+	//ch = 'a';
+	//printf("ch =%c\n",ch);
+	
+	char a;
+	int b;
+	short int c;
+	long int d;
+	float e = 3.8f;
+	double f = 5.6;
+
+	printf("sizeof(a) =%d\n", sizeof(a));
+	printf("sizeof(b) =%d\n", sizeof(b));
+	printf("sizeof(c) =%d\n", sizeof(c));
+	printf("sizeof(d) =%d\n", sizeof(d));
+	printf("sizeof(e) =%d\n", sizeof(e));
+	printf("sizeof(f) =%d\n", sizeof(f));
+	return 0;
+}
+
+//char(字符变量)
+//int(整型)
+//short int（短整型）
+//long int a（长整型）
+// float（浮点变量）实数
+//double（双浮点变量）
+//struct（）
+
+
+
+
+
